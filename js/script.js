e01a27d5428c8d7795a243c19378488f679649d1
MarcoDeRosso <mcoder@hotmail.it>
2021-06-10 15:15:17 +0200

    if added at while function

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ function numberGenerator(min, max) {
     var arrayNum = [];
     while (arrayNum.length < 16) {
         var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        arrayNum.push(randomNum)
+        if (randomNum !== arrayNum.length) {
+            arrayNum.push(randomNum)
+        }
 
     }
     console.log(arrayNum)
@@ -16,3 +18,6 @@ console.log(numberGenerator(1, 100))
 /*devo però generare 16 numeri, dovrò quindi utilizzare un ciclo che ripeta il generatore di numeri n volte fino a 16
 facendo però in modo che ogni numero generato venga inserito dentro un array vuoto, tramite un push. darò quindi al while, come condizione,
 di ripetersi 16 volte fino a che l'array.length non sarà pieno*/
+
+/*Devo però fare in modo che dentro l'array vengano messi solo numeri diversi, 
+qui SE il numero generato è diverso da quelli presenti dentro l'array, ALLORA PUSHALO */
