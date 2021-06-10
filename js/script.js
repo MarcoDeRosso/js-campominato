d1465027e0377c3f2618c5f0b237a5e37449e553
MarcoDeRosso <mcoder@hotmail.it>
2021-06-10 14:58:11 +0200

    16 num generated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-/*per prima cosa faccio creare al computer 16 numeri randomici, tramite una funzione, dandogli un minimo e un massimo come valore che saranno 1 e 100 */
+/*per prima cosa faccio creare al computer 16 numeri randomici, tramite una funzione, dandogli un 
+minimo e un massimo come valore che saranno 1 e 100 */
 
 function numberGenerator(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    var arrayNum = [];
+    while (arrayNum.length < 16) {
+        var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        arrayNum.push(randomNum)
+
+    }
+    console.log(arrayNum)
 }
+
+console.log(numberGenerator(1, 100))
+
+/*devo però generare 16 numeri, dovrò quindi utilizzare un ciclo che ripeta il generatore di numeri n volte fino a 16
+facendo però in modo che ogni numero generato venga inserito dentro un array vuoto, tramite un push. darò quindi al while, come condizione,
+di ripetersi 16 volte fino a che l'array.length non sarà pieno*/
