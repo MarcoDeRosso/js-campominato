679a31336972928854e5e62ec9c5a1ea86d3d83d
MarcoDeRosso <mcoder@hotmail.it>
2021-06-10 17:00:47 +0200

    bonus ok - challenge online

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 var output = document.getElementById("output")
+
+var userChallenge = parseInt(prompt("Scegli il livello di difficoltà: 0, 1, 2."))
+while (isNaN(userChallenge) || userChallenge < 1 || userChallenge > 2) {
+    alert("Attenzione i livelli di difficoltà sono: 0, 1, 2.")
+    var userChallenge = parseInt(prompt("Scegli il livello di difficoltà: 0, 1, 2."))
+}
+console.log(userChallenge);
+switch (userChallenge) {
+    case 0:
+        max = 100;
+        break;
+    case 1:
+        max = 80;
+        break;
+    case 2:
+        max = 50;
+        break;
+}
+console.log(max, "questo è il valore di max")
     /*per prima cosa faccio creare al computer 16 numeri randomici, tramite una funzione, dandogli un 
     minimo e un massimo come valore che saranno 1 e 100 */
 
@@ -15,7 +34,7 @@ function numberGenerator(min, max) {
     console.log(arrayNum)
 }
 
-console.log(numberGenerator(1, 100))
+console.log(numberGenerator(1, max))
 
 /*devo però generare 16 numeri, dovrò quindi utilizzare un ciclo che ripeta il generatore di numeri n volte fino a 16
 facendo però in modo che ogni numero generato venga inserito dentro un array vuoto, tramite un push. darò quindi al while, come condizione,
@@ -28,9 +47,9 @@ qui SE il numero generato è diverso da quelli presenti dentro l'array, ALLORA P
 Mi dovrò assicurare che l'utente inserisca solo un numero, che vada da 1 a 100 ed in caso contrario gli farò ripetere l'azione, 
 grazie ad un ciclo while  */
 var userArray = [];
-while (userArray.length < 84) {
+while (userArray.length < (max - 16)) {
     var userNum = parseInt(prompt("Tenta la FORTUNA! Inserisci un numero da 1 a 100, occhio a non ripetere lo stesso numero! ;)"))
-    while (isNaN(userNum) || userNum < 1 || userNum > 100) {
+    while (isNaN(userNum) || userNum < 1 || userNum > max) {
         alert("attenzione devi inserire un numero da 1 a 100");
         var userNum = parseInt(prompt("inserisci un numero da 1 a 100, occhio a non ripetere lo stesso numero! ;)"));
     }
@@ -51,3 +70,10 @@ il while avrà quindi come condizione la lungezza dell'array che dovrà essere m
 /*ora, dovrò fare in modo che l'utente, se scrive un numero dentro all'array generato dalla funzione perde il gioco, con break che interrompe il ciclo while
 devo anche scrivere una codizione che se l'utente ripete un numero dentro l'array utente, ha perso*/
 /*dichiaro il punteggio dell'utente prendendo il numero di "item" dentro l'array dell'utente */
+
+/*per il bonus devo chiedere all'utente tramite prompt di inserire un livello di difficoltà: 
+SE sceglie 0 allora il max della funzione che calcola i numeri random sarà 100
+SE sceglie 1 il max sarà 80
+SE sceglie 2 il max sarà 50
+
+userò il switch per determinare le varie condizioni*/
